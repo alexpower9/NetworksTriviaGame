1a7b283640f80c877213361dd7a563e35f59d4d5
HunterYocum <hunter.p.yocum@gmail.com>
2024-04-01 13:54:51 -0400

    New Type of Implementation

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,62 +1,62 @@
 package Server;
 
 import java.io.IOException;
-import java.net.DatagramSocket;
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import SwingWindow.AppWindow;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Server
-{   
-    private static ArrayList<ClientHandler> clients = new ArrayList<ClientHandler>();
-    
-    public static void printIP() throws IOException
-    {
-        Socket s = new Socket();
-        s.connect(new InetSocketAddress("google.com", 80));
-        String ip = s.getLocalAddress().getHostAddress();
-        s.close();
-        System.out.println("Current IP is " + ip);
+public class Server{
+
+    private ConcurrentLinkedQueue<Poll> pollQueue;
+    private ServerSocket serverSocket;
+    private boolean gameOver;
+    private int clientIDs;
+    private ExecutorService executorService = Executors.newCachedThreadPool();
+
+    public Server(int port){
+        gameOver = false;
+        pollQueue = new ConcurrentLinkedQueue<>();
+        this.clientIDs = 0;
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.println("Server is running");
+        } 
+        catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
     }
-    public static void main(String[] args)
-    {
-        try
-        {
-            //print ip
-            printIP();
 
-            //so we have tcp connection and udp connection
-            ServerSocket server = new ServerSocket(1234);
-            DatagramSocket udpSocket = new DatagramSocket(1234);
+    public void startServer(){      
 
-            
-
-            while(true) //accept clients
-            {
-                Socket socket = server.accept();
-                ClientHandler clientHandler = new ClientHandler(socket, udpSocket); //each clientHandler handles input/output for a client
-                clients.add(clientHandler);
-                new Thread(clientHandler).start();
-                System.out.println("New Client connected"); //this is working, which is good
-
-                //just testing to see if we can send a question to the client and have the client change states as we need it, which it does
-                for (ClientHandler client : clients)
-                {
-                    client.sendMessage("STATE:AWAITING_GAME_START");
-                    Thread.sleep(1000);
-                    client.sendQuestion("src/QuestionFiles/question1.txt");
-                }
-                
+        executorService.submit(new UDPThread(12345,pollQueue));
+        executorService.submit(new PollQueue(pollQueue));
+        
+        while (!gameOver){
+            Socket clientSocket = null;
+            try {
+                clientSocket = serverSocket.accept();
+                clientSocket.setReuseAddress(true);
+                System.out.println("A new client has connected: " + clientSocket);
+                clientIDs++;
+                System.out.println(clientIDs);
+                executorService.submit(new ClientHandler(clientSocket, clientIDs, pollQueue));
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-        catch (Exception e)
-        {
-            System.out.println("Error: " + e);
-        }
+
+    }
+
+
+
+
+    public static void main(String[] args){
         
+        Server server = new Server(1234);
+        server.startServer();
+
+
     }
 }
